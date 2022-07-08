101a65a0eb5fef3d57303a783b8f532257d43481
serhii.n <serhii.n@thescimus.com>
2022-07-08 23:45:31 +0300

    Update kernel and improve speed for MMC from 25Mbyte/S to 100Mbyte/S and WiFi from 100Mbit/S to 250Mbit/S

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-LINUX_VERSION=5.18
+LINUX_VERSION=5.18.9
 
 apt update
 apt full-upgrade -y
@@ -15,6 +15,7 @@ patch -p1 < /root/build/patches/0002-rename_phy_regulator_variable.patch
 patch -p1 < /root/build/patches/0003-add_support_for_enabling_regulator.patch
 patch -p1 < /root/build/patches/0004-enable_ethernet.patch
 patch -p1 < /root/build/patches/0005-ath_regd_optional.patch
+patch -p1 < /root/build/patches/0006-improve_mmc_and_wifi_speed.patch
 scripts/config --enable ATH_USER_REGD
 scripts/config --disable SECURITY_LOCKDOWN_LSM
 scripts/config --disable MODULE_SIG
